705cfb0e14502b355389949a403942e433178837
Chris Hassebrook <chasse20@gmail.com>
2019-07-07 23:59:45 -0400

    Update sql-backup.sh

--- sql-backup.sh
+++ sql-backup.sh
@@ -26,18 +26,18 @@ ROTATION_DATE=""
 
 if [ $1 == "h" ] && [ $ROTATION_HOURLY -gt 0 ]
 then
-        ROTATION_DATE=`date --date="$ROTATION_HOURLY hour ago" +%F_%H`
+	ROTATION_DATE=`date --date="$ROTATION_HOURLY hour ago" +%F_%H`
 elif [ $1 == "d" ] && [ $ROTATION_DAILY -gt 0 ]
 then
-        ROTATION_DATE=`date --date="$ROTATION_DAILY day ago" +%F_%H`
+	ROTATION_DATE=`date --date="$ROTATION_DAILY day ago" +%F_%H`
 elif [ $1 == "w" ] && [ $ROTATION_WEEKLY -gt 0 ]
 then
-        ROTATION_DATE=`date --date="$ROTATION_WEEKLY week ago" +%F_%H`
+	ROTATION_DATE=`date --date="$ROTATION_WEEKLY week ago" +%F_%H`
 elif [ $1 == "m" ] && [ $ROTATION_MONTHLY -gt 0 ]
 then
-        ROTATION_DATE=`date --date="$ROTATION_MONTHLY month ago" +%F_%H`
+	ROTATION_DATE=`date --date="$ROTATION_MONTHLY month ago" +%F_%H`
 else
-        exit 1
+	exit 1
 fi
 
 ############################
@@ -47,16 +47,16 @@ DATE=`date +%F_%H`
 
 for i in "${DB_NAMES[@]}"
 do
-        mysqldump -u $DB_USER -p$DB_PASSWORD $i | gzip > $LOCAL_DIR/$i-$1-$DATE.sql.gz
-        rm -f $LOCAL_DIR/$i-$1-$ROTATION_DATE.sql.gz
+	mysqldump -u $DB_USER -p$DB_PASSWORD $i | gzip > $LOCAL_DIR/$i-$1-$DATE.sql.gz
+	rm -f $LOCAL_DIR/$i-$1-$ROTATION_DATE.sql.gz
 
-        ftp -n $FTP_SERVER << END_SCRIPT
-        user "$FTP_USERNAME" "$FTP_PASSWORD"
-        binary
-        cd $FTP_DIR
-        lcd $LOCAL_DIR
-        put "$i-$1-$DATE.sql.gz"
-        delete "$i-$1-$ROTATION_DATE.sql.gz"
-        bye
+	ftp -n $FTP_SERVER << END_SCRIPT
+	user "$FTP_USERNAME" "$FTP_PASSWORD"
+	binary
+	cd $FTP_DIR
+	lcd $LOCAL_DIR
+	put "$i-$1-$DATE.sql.gz"
+	delete "$i-$1-$ROTATION_DATE.sql.gz"
+	bye
 END_SCRIPT
 done
